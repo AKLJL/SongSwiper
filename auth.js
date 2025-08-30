4b34a600f1a03808279f8ece3bc25cabd957de7b
AKLJL <74138056+AKLJL@users.noreply.github.com>
2025-08-30 15:12:46 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,22 +1,24 @@
 const CLIENT_ID = "2557a72b59e140fe98c86ec8e8ec5854";
-const REDIRECT_URI = "https://akljl.github.io/song-swiper/";
+const REDIRECT_URI = "https://akljl.github.io/song-swiper/index.html";
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const RESPONSE_TYPE = "token";
 
 document.getElementById("login-btn").addEventListener("click", () => {
-  window.location = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(
+  const authUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(
     REDIRECT_URI
-  )}&response_type=${RESPONSE_TYPE}&scope=playlist-read-private%20playlist-read-collaborative%20user-library-read%20playlist-modify-private%20playlist-modify-public`;
+  )}&response_type=${RESPONSE_TYPE}&scope=playlist-read-private%20playlist-read-collaborative%20user-read-private`;
+  window.location = authUrl;
 });
 
-// Get token from URL after redirect
+// Save token from URL after redirect
 window.onload = () => {
   const hash = window.location.hash;
   if (hash) {
-    const token = new URLSearchParams(hash.substring(1)).get("access_token");
+    const params = new URLSearchParams(hash.substring(1));
+    const token = params.get("access_token");
     if (token) {
       localStorage.setItem("spotify_token", token);
-      window.location = "playlists.html"; // ✅ send user to playlists page
+      window.location.href = "playlists.html";
     }
   }
 };
